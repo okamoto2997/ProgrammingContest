99cea2735d787c8dc5bed8294384b54ccd178b87
Mamoru OKAMOTO <okamoto6496@gmail.com>
2021-04-18 20:41:44 +0900

    create template

---
+++ template/test.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 path-to-executable"
+  exit 1
+fi
+
+
+DIR=`dirname $1`/
+EXE=`basename $1`
+
+cd ${DIR}
+
+if [ ! -e "${EXE}" ]; then
+  make ${EXE}
+fi
+
+find in -type f | while read input; do
+  output=`basename ${input} .in`.out
+  ./${EXE} < ${input} > out/${output}
+done
