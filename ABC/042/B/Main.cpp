cd175114538e46accb71ac5dfa03e48a78a33350
Mamoru OKAMOTO <okamoto6496@gmail.com>
2021-04-19 02:29:06 +0900

    register

--- ABC/042/B/Main.cpp
+++ ABC/042/B/Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <ctime>
 #include <filesystem>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 #include "../lib/log.hpp"
 
 using namespace std;
 
 int main(){
+  int N, L;
+  cin >> N >> L;
+  vector<string> S(N);
+  for (auto& s : S) cin >> s;
+  sort(S.begin(), S.end());
+  for (auto& s : S) cout << s;
+  cout << endl;
   return 0;
 }
