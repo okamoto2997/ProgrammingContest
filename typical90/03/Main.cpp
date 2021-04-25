ef2a0247d040f5564bb77cbaf269d77a2c146403
Mamoru OKAMOTO <okamoto6496@gmail.com>
2021-04-25 19:31:57 +0900

    更新

---
+++ typical90/03/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+
+#include "lib/log.hpp"
+#include "lib/utility.hpp"
+
+using namespace std;
+using namespace lib;
+
+int main(){
+  int N = read();
+  map<int, int> antipodal;
+  set<int> semi_antipodal;
+  return 0;
+}
