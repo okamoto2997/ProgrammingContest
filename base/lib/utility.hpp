435635c1611d3035da9496fc445bdec6ea89b4c8
Mamoru OKAMOTO <okamoto6496@gmail.com>
2021-04-25 16:36:10 +0900

    register typical90/01

--- base/lib/utility.hpp
+++ base/lib/utility.hpp
@@ -1,49 +1,50 @@
 #pragma once
 
-namespace lib {
+namespace lib{
 
   using i64 = int64_t;
   using u64 = uint64_t;
 
-  constexpr u64 gcd(u64 m, u64 n) {
+  constexpr u64 gcd(u64 m, u64 n){
     return m == n ? m : m < n ? (n % m == 0 ? m : gcd(n % m, m)) : (m % n == 0 ? n : gcd(n, m % n));
-
-    std::string readline(std::istream & src = std::cin) {
-      std::string line;
-      std::getline(src, line);
-      return line;
-    }
-
-    template<class T = int>
-    T read(std::istream & src = std::cin) {
-      T buf;
-      src >> buf;
-      return buf;
-    }
-
-    template<class T = int>
-    std::vector<T> read_vector(size_t size, std::istream & src = std::cin) {
-      std::vector<T> buf(size);
-      for (auto& b : buf) src >> b;
-      return buf;
-    }
-
-    template<size_t INDEX, class T>
-    T& read_elem(T & buf, std::istream&) {
-      return buf;
-    }
-
-    template<size_t INDEX, class T, class F, class... After>
-    T& read_elem(T & buf, std::istream & src = std::cin) {
-      std::get<INDEX>(buf) = read<F>(src);
-      return read_elem<INDEX + 1, T, After...>(buf, src);
-    }
-
-    template<class... T>
-    std::tuple<T...> read_elems(std::istream & src = std::cin) {
-      std::tuple<T...> buf;
-
-      return read_elem<0, typeof(buf), T...>(buf, src);
-    }
-
   }
+
+  std::string readline(std::istream& src = std::cin){
+    std::string line;
+    std::getline(src, line);
+    return line;
+  }
+
+  template<class T = int>
+  T read(std::istream& src = std::cin){
+    T buf;
+    src >> buf;
+    return buf;
+  }
+
+  template<class T = int>
+  std::vector<T> read_vector(size_t size, std::istream& src = std::cin){
+    std::vector<T> buf(size);
+    for (auto& b : buf) src >> b;
+    return buf;
+  }
+
+  template<size_t INDEX, class T>
+  T& read_elem(T& buf, std::istream&){
+    return buf;
+  }
+
+  template<size_t INDEX, class T, class F, class... After>
+  T& read_elem(T& buf, std::istream& src = std::cin){
+    std::get<INDEX>(buf) = read<F>(src);
+    return read_elem<INDEX + 1, T, After...>(buf, src);
+  }
+
+  template<class... T>
+  std::tuple<T...> read_elems(std::istream& src = std::cin){
+    std::tuple<T...> buf;
+
+    return read_elem<0, typeof(buf), T...>(buf, src);
+  }
+
+}
--- typical90/01/upload.cpp
+++ typical90/01/upload.cpp
@@ -4,20 +4,52 @@
 #include <algorithm>
 #include <numeric>
 #include <cmath>
-
-#include "lib/log.hpp"
-#include "lib/utility.hpp"
-
+namespace lib{
+  using i64 = int64_t;
+  using u64 = uint64_t;
+  constexpr u64 gcd(u64 m, u64 n){
+    return m == n ? m : m < n ? (n % m == 0 ? m : gcd(n % m, m)) : (m % n == 0 ? n : gcd(n, m % n));
+  }
+  std::string readline(std::istream& src = std::cin){
+    std::string line;
+    std::getline(src, line);
+    return line;
+  }
+  template<class T = int>
+  T read(std::istream& src = std::cin){
+    T buf;
+    src >> buf;
+    return buf;
+  }
+  template<class T = int>
+  std::vector<T> read_vector(size_t size, std::istream& src = std::cin){
+    std::vector<T> buf(size);
+    for (auto& b : buf) src >> b;
+    return buf;
+  }
+  template<size_t INDEX, class T>
+  T& read_elem(T& buf, std::istream&){
+    return buf;
+  }
+  template<size_t INDEX, class T, class F, class... After>
+  T& read_elem(T& buf, std::istream& src = std::cin){
+    std::get<INDEX>(buf) = read<F>(src);
+    return read_elem<INDEX + 1, T, After...>(buf, src);
+  }
+  template<class... T>
+  std::tuple<T...> read_elems(std::istream& src = std::cin){
+    std::tuple<T...> buf;
+    return read_elem<0, typeof(buf), T...>(buf, src);
+  }
+}
 using namespace std;
 using namespace lib;
-
 int score(const vector<int> cut, int L, const vector<int>& A){
   int ret = A[cut[0]];
   for (int i = 1; i < cut.size() - 1; ++i)
     ret = min(ret, A[cut[i]] - A[cut[i - 1]]);
   ret = min(ret, L - A[cut.back()]);
 }
-
 bool is_cut_possibile(int score, int K, int L, const vector<int>& A){
   int current_pos = 0;
   int current_index = 0;
@@ -32,7 +64,6 @@ bool is_cut_possibile(int score, int K, int L, const vector<int>& A){
   }
   return L - current_pos >= score;
 }
-
 int main(){
   auto [N, L] = read_elems<int, int>();
   int K = read();
@@ -40,7 +71,6 @@ int main(){
   vector<int> cut(K); iota(cut.begin(), cut.end(), 0);
   int min_score = 0;
   int sup_score = L;
-
   while (sup_score - min_score > 1){
     int candidate = (sup_score + min_score) / 2;
     cerr << "candidate: " << candidate << "\n";
@@ -49,8 +79,6 @@ int main(){
     else
       sup_score = candidate;
   }
-
   cout << min_score << endl;
-
   return 0;
 }
