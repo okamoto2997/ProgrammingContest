dd13b3d22aab7d634a0c5b2aed51d1a15a161456
Mamoru OKAMOTO <okamoto6496@gmail.com>
2021-04-25 12:24:14 +0900

    改良

--- base/test.sh
+++ base/test.sh
@@ -1,7 +1,44 @@
 #/bin/sh
 
-DIR=./
-EXE=Main
+print_usage(){
+  cat <<EOF
+Usage: ${0} [-d dir] [-e executable] [-h]
+  -d dir
+    directorty contining executable. if -d is not set, obtained from -e
+  -e executable
+    executable to test. if -e is not set, "-e ./Main" is assumed.
+  -h
+    print this message and exit.
+EOF
+}
+
+DIR=
+EXE=./Main
+
+while [ -n "$*" ]; do
+  OPT=$1
+  case "$OPT" in
+    "-d" )
+      DIR=$2
+      shift
+      ;;
+    "-e" )
+      EXE=$2
+      shift
+      ;;
+    "-h" )
+      print_usage
+      exit
+      ;;
+  esac
+  shift
+done
+
+if [ -z "$DIR" ]; then
+  DIR=`dirname ${EXE}`
+fi
+
+EXE=`basename ${DIR}/${EXE}`
 
 cd ${DIR}
 
