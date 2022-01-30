d2b99918c4102fc31997f203ba2e7eac98178b96
Mamoru OKAMOTO <okamoto6496@gmail.com>
2022-01-31 01:30:22 +0900

    ABC/012/D

--- ABC/012/D/Main.cpp
+++ ABC/012/D/Main.cpp
@@ -1,10 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <set>
-#include <map>
 #include <algorithm>
 #include <cmath>
+#include <cstdarg>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <sys/time.h>
+#include <vector>
 
 #include "lib/log.hpp"
 #include "lib/utility.hpp"
@@ -12,6 +14,32 @@
 using namespace std;
 using namespace lib;
 
-int main(){
+int main() {
+  auto [N, M] = read_elems<int, int>();
+  vector<vector<int>> d(N, vector<int>(N, 3000000));
+  for (int i = 0; i < N; ++i)
+    d[i][i] = 0;
+  for (int i = 0; i < M; ++i) {
+    auto [a, b, t] = read_elems<int, int, int>();
+    --a, --b;
+    d[a][b] = d[b][a] = t;
+  }
+
+  /// @comment Warshall-Floyd method
+  for (int k = 0; k < N; ++k)
+    for (int i = 0; i < N; ++i)
+      for (int j = 0; j < N; ++j)
+        d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+
+  int minmax = 3000000;
+  for (int i = 0; i < N; ++i) {
+    int max = 0;
+    for (int j = 0; j < N; ++j)
+      max = std::max(max, d[i][j]);
+    minmax = min(minmax, max);
+  }
+
+  cout << minmax << endl;
+
   return 0;
 }
