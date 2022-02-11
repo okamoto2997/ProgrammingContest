d4c90733b180edda7299e02a61ac3ef1819dac6a
Mamoru OKAMOTO <okamoto6496@gmail.com>
2022-02-12 00:29:07 +0900

    update

--- base/make_upload.sh
+++ base/make_upload.sh
@@ -24,19 +24,38 @@ while [ -n "$*" ]; do
   shift
 done
 
-
-
 echo "Target: ${SOURCE}"
 
 DIR=`dirname $SOURCE`
-SYSTEM_H=`mktemp make_upload.system.h.XXXXXX`
-CONTENT=`mktemp make_upload.content.XXXXXX`
-trap 'rm -f ${SYSTEM_H} ${CONTENT}' EXIT
+DIR=`cd ${DIR}; pwd`
 
-sed -n "/^#include *<.*>/p" < ${SOURCE} > ${SYSTEM_H}
-sed "/^#include *<.*>/d" < ${SOURCE} > ${CONTENT}
+WORK=`mktemp -d tmp.upload.XXXXXX`
+WORK=`cd ${WORK}; pwd`
+FILE_LIST=${WORK}/file_list
 
-g++ -E -x c++ -I${DIR} -I${DIR}/../ -DNDEBUG ${CONTENT} | sed "/^#/d" | sed "/^ *$/d" >> ${SYSTEM_H}
+g++ -MM -I. -std=gnu++17 -Wall -Wextra -O2 ${SOURCE} \
+  | tr -d "\n" | tr -d '\\' | sed -e "s/[a-zA-Z.]\+://" > ${FILE_LIST}
+
+cd ${WORK}
+
+SYSTEM_H=`mktemp system_headers.XXXXXX`
+trap 'rm -rf ${WORK} a.out' EXIT
+
+for file in `cat ${FILE_LIST}`; do
+  mkdir -p `dirname ${file}`
+  sed -n "/^#include *<.*>/p" < ${DIR}/${file} >> ${SYSTEM_H}
+  sed "/^#include *<.*>/d" < ${DIR}/${file} > ${file}
+done
+
+g++ -E -x c++ -I${DIR} -I${DIR}/../ -DNDEBUG ${SOURCE} | sed "/^#/d" | sed "/^ *$/d" >> ${SYSTEM_H}
 
 mv ${SYSTEM_H} ${DIR}/upload.cpp
+
+cd -
+echo "compile test."
+
+g++ -std=gnu++17 -Wall -Wextra -O2 -DONLINE_JUDGE -o ./a.out ./upload.cpp
+
+echo "complete."
+
 code ${DIR}/upload.cpp
